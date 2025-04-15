ac4d95b710da6920f54fbe895528399a022fc0cd
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-04-15 03:52:07 +0000

    [CORRECCION ENTREGA GENERADOR DE DOMINIOS] src/app.js: inserto codigo para presentar en html app_1.js: es el codigo para correr en terminal

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dom = ["com", "net", "us", "io", "es", "edu", "gov"];
+
+  let dominios = [];
+  let count = 0
+
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let x in dom) {
+          const node = document.createElement("li");
+          node.className="list-group-item"
+          node.innerText = (pronoun[i] + adj[j] + noun[k] + "." + dom[x]);
+          //node.appendChild(textnode);
+          document.getElementById("lista_dominios").appendChild(node);
+          //console.log(pronoun[i] + adj[j] + noun[k] + "." + dom[x]);
+        }
+      }
+    }
+  }
+
 };
